8811cd30480299cdcec0ca7da3793aaa75d6fa22
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2023-05-01 11:06:11 +0300

    fix: hifi config

--- recipes/jhlfrfufyfn/train_hifigan.py
+++ recipes/jhlfrfufyfn/train_hifigan.py
@@ -18,14 +18,18 @@ audio_config = BaseAudioConfig(
 )
 
 config = HifiganConfig(
-    batch_size=64,
-    eval_batch_size=32,
+    batch_size=128,
+    eval_batch_size=64,
     num_loader_workers=8,
     num_eval_loader_workers=8,
+    lr_disc=0.0004,
+    lr_gen=0.0004,
     run_eval=True,
     test_delay_epochs=5,
     epochs=1000,
     use_noise_augment=True,
+    seq_len=8192,
+    pad_short=2000,
     save_step=5000,
     print_step=50,
     print_eval=True,
